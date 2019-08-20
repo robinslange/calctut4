52f962b591dbd6dbc7813d2daeee57d85670e06d
Robin Lange <robinsimonlange@gmail.com>
2019-08-21 00:00:09 +1200

    subtract test added

--- src/test/java/calctut4/CalcTest.java
+++ src/test/java/calctut4/CalcTest.java
@@ -2,6 +2,8 @@ package calctut4;
 
 import static org.junit.Assert.assertEquals;
 
+import org.junit.Test;
+
 import calctut4.Calc;
 
 public class CalcTest 
@@ -12,6 +14,13 @@ public class CalcTest
         Calc newCalc = new Calc();
         int result = newCalc.add(1,1);
         assertEquals(2, result);
+    }
+
+    @org.junit.Test
+    public void subtract() {
+        Calc newCalc = new Calc();
+        int result = newCalc.subtract(2, 1);
+        assertEquals(1, result);
     }
     
     
