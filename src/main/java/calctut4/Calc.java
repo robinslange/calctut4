10a3000307fabc1ae09efa044c631a928fe9cfe9
Robin Lange <robinsimonlange@gmail.com>
2019-08-21 00:28:14 +1200

    formatting fix

--- src/main/java/calctut4/Calc.java
+++ src/main/java/calctut4/Calc.java
@@ -5,7 +5,7 @@ public class Calc {
     public int add(int num1, int num2) {
         return num1 + num2;
     }
-    public int subtract( int num1, int num2) {
+    public int subtract(int num1, int num2) {
         return num1 - num2;
     } 
     public int multiply (int num1, int num2) {
